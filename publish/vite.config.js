b6f59f4abd82c4bc42c664c79b2af1596070b70e
adityarizqi <adityarizqi1010@gmail.com>
2026-01-03 11:41:42 +0700

    First Commit

---
+++ publish/vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import tailwindcss from '@tailwindcss/vite';
+
+export default defineConfig({
+    plugins: [
+        tailwindcss(),
+        laravel({
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+                'vendor/indolat/filament-simple-theme/resources/css/theme.css'
+            ],
+            refresh: true,
+        }),
+    ],
+});
